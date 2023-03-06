b98625822d5ab238c1a91a2d7f1fa312593b691c
appscriplaxminarayana <88077481+appscriplaxminarayana@users.noreply.github.com>
2023-03-07 00:40:28 +0530

    Update index.js

--- src/services/SubCategories/index.js
+++ src/services/SubCategories/index.js
@@ -70,7 +70,7 @@ const getSubCategoryByCategory = async (req, res, next) => {
             if (Category.length === 0) {
                 res.status(404).json({ message: "Not found Category with id " + id });
             } else {
-                res.status(200).json(Category);
+                res.status(200).json( { message:"Success", data: Category } );
             }
         } catch (err) {
             res.status(500).json({ message: "Error retrieving Category with id " + id });
